7a6254a37c65f9585ad77296c81c4d3906b3c4cf
Emanuel <emanuellucietti@tdf.edu.ar>
2022-09-12 17:18:10 -0300

    ejercicio clase 39

--- primero.ts
+++ primero.ts
@@ -1,4 +1,3 @@
 console.log("hola");
-let readlineSync = require('readline-sync');
-let alturaPersona = readlineSync.question("Indique la altura de la persona: ");
-console.log(alturaPersona);
+
+const Font = require('ascii-art-font'); 
