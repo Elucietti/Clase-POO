642b8655f3bbef3a87fef18684785af722f447ad
Emanuel <emanuellucietti@tdf.edu.ar>
2022-09-14 22:23:11 -0300

    ejercicio clase 40

--- clase-40/pava.ts
+++ clase-40/pava.ts
@@ -1,19 +1,10 @@
-//pava electrica
-//  funcionalidades
-//        calentar agua para mate
-//        hervir agua
-
-//  estado
-//      encendido/apagado
-//      calentando/hirviendo
-
 class Pava{
     //atributos o estados
     encendido:boolean;
     calentando:number=0;
     hirviendo:number=0;
 
-}    
+   
 
 //funcionalidades
 
@@ -24,4 +15,5 @@ mate(){
 
 cafe(){
     console.log("agua hervida");
-}
+  }
+} 
