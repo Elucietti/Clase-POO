19115a61d1abeccea303cb327f19f59d08aa559f
Emanuel <emanuellucietti@tdf.edu.ar>
2022-09-14 23:35:42 -0300

    ejercicio clase 40

---
+++ pava.js
@@ -0,0 +1,25 @@
+var Pava = /** @class */ (function () {
+    //funcionalidades
+    function Pava(encendido, calentando, hirviendo) {
+        this.calentando = 0;
+        this.hirviendo = 0;
+        this.encendido = true;
+        this.calentando = calentando;
+        this.hirviendo = hirviendo;
+    }
+    Pava.prototype.funcionando = function () {
+        console.log("luz indicadora");
+    };
+    Pava.prototype.mate = function () {
+        console.log("calentando a 75° ");
+    };
+    Pava.prototype.cafe = function () {
+        console.log("agua hervida");
+    };
+    return Pava;
+}());
+var pava = new Pava(true, 74, 100);
+pava.funcionando();
+pava.mate();
+console.log("temperatura del agua " + pava.calentando + "°");
+console.log("temperatura del agua " + pava.hirviendo + "°");
---
+++ pava.ts
@@ -0,0 +1,39 @@
+class Pava{
+    //atributos o estados
+    encendido:boolean;
+    calentando:number=0;
+    hirviendo:number=0;
+
+   
+//funcionalidades
+
+constructor(encendido:boolean, calentando:number, hirviendo:number){
+    this.encendido=true;
+    this.calentando=calentando;
+    this.hirviendo=hirviendo;
+
+
+}
+
+
+funcionando(){
+
+    console.log("luz indicadora");
+}
+
+mate(){
+
+    console.log("calentando a 75° ");
+}
+
+cafe(){
+    console.log("agua hervida");
+  }
+} 
+
+let pava = new Pava(true, 74, 100 );
+pava.funcionando();
+pava.mate();
+console.log("temperatura del agua " + pava.calentando + "°");
+console.log("temperatura del agua " + pava.hirviendo + "°")
+    
