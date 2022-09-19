4c4aa47fd598281b567d0df75b64ed85b1829956
Emanuel <emanuellucietti@tdf.edu.ar>
2022-09-19 15:42:09 -0300

    ejercicio clase 41

--- clase-41/electrodomestico.js
+++ clase-41/electrodomestico.js
@@ -1,53 +1,52 @@
-var lavaropa = /** @class */ (function () {
-    function lavaropa() {
+"use strict";
+exports.__esModule = true;
+var lavarropa = /** @class */ (function () {
+    function lavarropa(parametroEncendido, parametroLavEco, parametroLavInt, parametroCentrifugado) {
+        this.encendido = parametroEncendido;
+        this.lavadoEcologico = parametroLavEco;
+        this.lavadoIntenso = parametroLavInt;
+        this.centrifugado = parametroCentrifugado;
     }
-    return lavaropa;
+    lavarropa.prototype.estado = function () {
+        if (this.encendido === true) {
+            var on = this.encendido = false;
+            return this.encendido;
+        }
+        else {
+            var off_1 = this.encendido = true;
+            return this.encendido;
+        }
+    };
+    lavarropa.prototype.setLavEco = function (lavUsuario) {
+        this.lavadoEcologico = lavUsuario;
+    };
+    lavarropa.prototype.lavEco = function () {
+        return this.lavadoEcologico;
+    };
+    lavarropa.prototype.setLavInt = function (lavUsuario2) {
+        this.lavadoIntenso = lavUsuario2;
+    };
+    lavarropa.prototype.lavInt = function () {
+        return this.lavadoIntenso;
+    };
+    lavarropa.prototype.setCentri = function (tiempoCentri) {
+        this.centrifugado = tiempoCentri;
+    };
+    lavarropa.prototype.centri = function () {
+        return this.centrifugado;
+    };
+    return lavarropa;
 }());
-constructor(parametroEncendido, boolean, parametroLavEco, boolean, parametroLavInt, boolean, parametroCentrifugado, boolean);
-{
-    this.encendido = parametroEncendido;
-    this.lavadoEcologico = parametroLavEco;
-    this.lavadoIntenso = parametroLavInt;
-    this.centrifugado = parametroCentrifugado;
-}
-estado();
-void {
-    "if": function (encendido) { }
-} === true;
-{
-    this.encendido = false;
-}
-{
-    this.encendido = true;
-}
-seleccionModoEco();
-void {
-    "if": function (lavadoEcologico) { }
-} === true;
-{
-    this.lavadoEcologico = false;
-}
-{
-    this.lavadoEcologico = true;
-}
-seleccionModoint();
-void {
-    "if": function (lavadoIntenso) { }
-} === true;
-{
-    this.lavadoIntenso = false;
-}
-{
-    this.lavadoIntenso = true;
-}
-seleccionModoint();
-void {
-    "if": function (centrifugado) { }
-} === true;
-{
-    this.centrifugado = false;
-}
-{
-    this.centrifugado = true;
-}
-var lavaropaDream = new lavaropa(false, false, false, false);
+var lavDream = new lavarropa(true, 0, 0, 0);
+lavDream.estado();
+var estados = lavDream.estado();
+lavDream.setLavEco(30);
+var tiempoEco = lavDream.lavEco();
+lavDream.setLavInt(0);
+var tiempoInt = lavDream.lavInt();
+lavDream.setCentri(15);
+var tiempoCentri = lavDream.centri();
+console.log("encendido:  " + estados);
+console.log("tiempo lavado ecologico: " + tiempoEco + " minutos");
+console.log("tiempo lavado intenso: " + tiempoInt + " minutos");
+console.log("centrifugado: " + tiempoCentri + " minutos");
--- clase-41/electrodomestico.ts
+++ clase-41/electrodomestico.ts
@@ -1,3 +1,5 @@
+import { off } from "process";
+
 class lavarropa{
     private encendido:boolean;
     private lavadoEcologico:number;
@@ -11,6 +13,62 @@ constructor(parametroEncendido:boolean, parametroLavEco:number, parametroLavInt:
     this.centrifugado=parametroCentrifugado;
 }
 
+public estado():boolean{
+    if(this.encendido===true){
+        let on:boolean=this.encendido=false;
+        return this.encendido;
+    
+    }else{
+        let off:boolean = this.encendido=true;
+        return this.encendido;
+    }
+}
 
+public setLavEco(lavUsuario:number):void{
+    this.lavadoEcologico=lavUsuario; 
+}
+
+public lavEco():number{
+    return this.lavadoEcologico;
+}
+
+public setLavInt(lavUsuario2:number):void{
+    this.lavadoIntenso=lavUsuario2;
+}
+
+public lavInt():number{
+    return this.lavadoIntenso;
+}
+
+public setCentri(tiempoCentri:number):void{
+    this.centrifugado=tiempoCentri
+}
+
+public centri():number{
+    return this.centrifugado;
+}
 
 }
+
+let lavDream = new lavarropa (true, 0, 0, 0);
+
+lavDream.estado();
+let estados:boolean=lavDream.estado();
+
+lavDream.setLavEco(30);
+let tiempoEco:number=lavDream.lavEco();
+
+lavDream.setLavInt(0);
+let tiempoInt:number=lavDream.lavInt();
+
+lavDream.setCentri(15);
+let tiempoCentri:number=lavDream.centri();
+
+
+
+console.log("encendido:  "+ estados);
+console.log("tiempo lavado ecologico: "+ tiempoEco + " minutos");
+console.log("tiempo lavado intenso: "+ tiempoInt + " minutos");
+console.log("centrifugado: "+ tiempoCentri + " minutos");
+
+
