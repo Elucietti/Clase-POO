89f4bb9488197b320a9a689ce2ff6d5e536bd7ae
Emanuel <emanuellucietti@tdf.edu.ar>
2022-09-18 22:51:01 -0300

    ejercicio clase 41

---
+++ clase-41/electrodomestico.js
@@ -0,0 +1,53 @@
+var lavaropa = /** @class */ (function () {
+    function lavaropa() {
+    }
+    return lavaropa;
+}());
+constructor(parametroEncendido, boolean, parametroLavEco, boolean, parametroLavInt, boolean, parametroCentrifugado, boolean);
+{
+    this.encendido = parametroEncendido;
+    this.lavadoEcologico = parametroLavEco;
+    this.lavadoIntenso = parametroLavInt;
+    this.centrifugado = parametroCentrifugado;
+}
+estado();
+void {
+    "if": function (encendido) { }
+} === true;
+{
+    this.encendido = false;
+}
+{
+    this.encendido = true;
+}
+seleccionModoEco();
+void {
+    "if": function (lavadoEcologico) { }
+} === true;
+{
+    this.lavadoEcologico = false;
+}
+{
+    this.lavadoEcologico = true;
+}
+seleccionModoint();
+void {
+    "if": function (lavadoIntenso) { }
+} === true;
+{
+    this.lavadoIntenso = false;
+}
+{
+    this.lavadoIntenso = true;
+}
+seleccionModoint();
+void {
+    "if": function (centrifugado) { }
+} === true;
+{
+    this.centrifugado = false;
+}
+{
+    this.centrifugado = true;
+}
+var lavaropaDream = new lavaropa(false, false, false, false);
---
+++ clase-41/electrodomestico.ts
@@ -0,0 +1,16 @@
+class lavarropa{
+    private encendido:boolean;
+    private lavadoEcologico:number;
+    private lavadoIntenso:number;
+    private centrifugado:number;
+
+constructor(parametroEncendido:boolean, parametroLavEco:number, parametroLavInt:number, parametroCentrifugado:number){
+    this.encendido=parametroEncendido;
+    this.lavadoEcologico=parametroLavEco;
+    this.lavadoIntenso=parametroLavInt;
+    this.centrifugado=parametroCentrifugado;
+}
+
+
+
+}
