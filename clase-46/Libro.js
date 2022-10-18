f424bea152fbf9307b74248786725e2e67d94fe2
Emanuel <emanuellucietti@tdf.edu.ar>
2022-10-18 07:32:04 -0300

    ejercicio clase 46

---
+++ clase-46/Libro.js
@@ -0,0 +1,27 @@
+"use strict";
+exports.__esModule = true;
+exports.Libro = void 0;
+var Libro = /** @class */ (function () {
+    function Libro(ptitulo, pAutor, pGenero) {
+        this.titulo = ptitulo;
+        this.autor = pAutor;
+        if (pGenero != undefined)
+            this.genero = pGenero;
+    }
+    Libro.prototype.getTitulo = function () {
+        return this.titulo;
+    };
+    Libro.prototype.getAutor = function () {
+        return this.autor;
+    };
+    Libro.prototype.getGenero = function () {
+        if (this.genero != undefined)
+            return this.genero;
+        return "";
+    };
+    Libro.prototype.toString = function () {
+        return 'titulo: ' + this.titulo + ' autor: ' + this.autor + ' genero: ' + this.genero;
+    };
+    return Libro;
+}());
+exports.Libro = Libro;
---
+++ clase-46/Libro.ts
@@ -0,0 +1,27 @@
+export class Libro{
+   private titulo:string;
+   private autor:string;
+   private genero:string;
+
+    constructor(ptitulo:string,pAutor:string,pGenero?:string){
+        this.titulo=ptitulo;
+        this.autor=pAutor;
+        if(pGenero != undefined)
+            this.genero=pGenero;
+    }
+
+    getTitulo():string{
+        return this.titulo;
+    }
+    getAutor():string{
+        return this.autor;
+    }
+    getGenero():string{
+        if(this.genero!=undefined)
+            return this.genero;
+        return "";    
+    }
+    toString():string{
+        return 'titulo: '+this.titulo+' autor: '+this.autor+' genero: '+this.genero;
+    }
+}
